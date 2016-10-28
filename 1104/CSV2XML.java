e9b4afb2b081ed2b8ec01d9408de83c6f988f6f1
capacitypp <aoi0x4c4f5645@gmail.com>
2016-10-28 15:39:40 +0900

    [update] Add comments

--- 1104/CSV2XML.java
+++ 1104/CSV2XML.java
@@ -58,19 +58,26 @@ public class CSV2XML {
 		System.out.println(fileName + "に出力しました。");
 	}
 
+	/* JSON形式の出力を生成し、ファイルに出力する */
 	public void saveJSON(String fileName, ArrayList<ArrayList<String>> retrieved) throws IOException, JSONException {
 		FileWriter fw = new FileWriter(new File(fileName));
 		JSONWriter writer = new JSONWriter(fw);
 		writer.object();
 		for (String label : evalLabel) {
+			/* unacc, accなどを書き出す */
 			writer.key(label);
+			/* 各evalにおけるarrayの開始 */
 			writer.array();
 			for (ArrayList<String> list : retrieved) {
+				/* 検索結果のうち、特定のevalのもののみを見る */
 				if (!list.get(list.size() - 1).equals(label))
 					continue;
+				/* 1つのCarの出力の開始 */
 				writer.object();
 				for (int i = 0; i < list.size() - 1; i++) {
+					/* buying,safetyなどを書き出す */
 					writer.key(keys[i]);
+					/* keyに対応するvalueを書き出す */
 					writer.value(list.get(i));
 				}
 				writer.endObject();
@@ -78,6 +85,7 @@ public class CSV2XML {
 			writer.endArray();
 		}
 		writer.endObject();
+		/* ストリームのフラッシュ */
 		fw.flush();
 
 		System.out.println(fileName + "に出力しました。");
@@ -90,17 +98,22 @@ public class CSV2XML {
 		String cmd[] = args[0].split("=");
 		String logic = null;
 		query.put(cmd[0], cmd[1]);
+		/* 条件が2個以上ある場合 */
 		if (args.length > 2) {
 			logic = args[1];
+			/* 3個目以降の条件は無視する */
 			String cmd2[] = args[2].split("=");
 			query.put(cmd2[0], cmd2[1]);
 		}
 
 		Car car = new Car("car.csv");
+		/* 検索 */
 		ArrayList<ArrayList<String>> retrieved = car.analyze(query, logic);
 		try {
+			/* XMLの出力 */
 			Document doc = xml.createDoc(retrieved);
 			xml.saveXML("car.xml", doc);
+			/* JSONの出力 */
 			xml.saveJSON("car.json", retrieved);
 		}
 		catch (Exception e) {
